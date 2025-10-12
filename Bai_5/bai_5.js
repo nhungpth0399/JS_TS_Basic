12b5a3b8762a3aa5bd9e8bcf1edbba1d50e02867
nhungpth0399 <85088019+nhungpth0399@users.noreply.github.com>
2025-10-12 23:30:28 +0700

    push new code

--- Bai_5/bai_5.js
+++ Bai_5/bai_5.js
@@ -1,7 +1,7 @@
 // function(tham so){}
 
 
-
+// bieu thuc ham
 // const tenBien = function(thamso){
 //     return ket qua
 // }
@@ -13,7 +13,24 @@ const tinhHieu = function(a, b){
 const ketQua = tinhHieu(8, 5)
 console.log(ketQua);
 
+// arrow function
+// const tenBien = (thamso) =>{
+//     return ketQua
+// }
+// neu ham don gian co the bo dau {} const bien = (thamso) => a * b
 
+const tinhNhan = (a, b) =>{ 
+    return a * b
+}
 
+console.log(tinhNhan(2, 3));
+
+// co the goi function truoc khi define (hoisting)
+
+console.log(tong(1, 4));
+
+function tong(a, b){
+    return a + b
+}
 
 
--- Bai_7/bai_7.js
+++ Bai_7/bai_7.js
@@ -1,9 +1,46 @@
 
 
-//
+//class trong JS
+// nguoi khai bao class hay su dung class goi la instance => giong nhu nguoi su dung ban thiet ke
+
+// class TenClass {
+//     //1 class fields (khai bao thuoc tinh mac dinh (co the co hoac khong))
+//     tenThuocTinhMacDinh = 'gia tri'
+
+//     //2 ham khoi tao (Constructor)
+//     constructor(thamso1){
+//         this.thuocTinh1 = thamso1
+//     }
+
+//     //3 Method (phuong thuc - hanh vi)
+//     tenPhuongThuc(){
+
+//     }
+// }
 
 
 
+class Car{
+    //1 class field
+    inEngineOn = false
+    
+    //constructor: nhan cac thong tin rieng cua tung chiec xe
+    constructor(brand, color){
+        this.brand = brand
+        this.color = color
+    }
+
+    //3
+    startEngine = () =>{
+        this.inEngineOn = true
+        console.log(`Dong co xe ${this.brand} da duoc khoi dong`);
+        
+    }
+    displayBrand = () =>{
+        console.log(`Day la chiec xe cua ${this.brand} mau ${this.color}`);
+        
+    }
+}
 
 
 
