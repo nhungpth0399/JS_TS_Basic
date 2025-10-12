8acf3f7ea5136dc730ed76357f80e409fdb7bc2a
nhungpth0399 <85088019+nhungpth0399@users.noreply.github.com>
2025-10-13 00:24:41 +0700

    new bai 7

--- Bai_7/bai_7.js
+++ Bai_7/bai_7.js
@@ -107,8 +107,84 @@ myAccount.deposit(100)
 
 console.log(myAccount.getBalance());
 
+// 2. Tinh triu tuong: hien thi cho nguoi dung biet nguoi dung co the lam gi, chu ko can biet lam nhu the nao
+// VD: Dieu khien TV.
+
+class CoffeeMachine {
+    #cleanmachine(){
+        console.log(`Ve sinh may`);
+        
+    }
+    makeCoffee(){
+        this.#cleanmachine()
+        console.log(`Ca phe da san sang!`);
+        
+    }
+}
+
+const machine = new CoffeeMachine()
+//nguoi dung chi can biet dung ham nay
+machine.makeCoffee()
 
 
+// 3. Tinh ke thua: class con co the ke thua class cha (toi uu code)
+// Dung tu khoa extend de ke thua
+// Dung super(bien) de goi tu ham cha
+// Ke thua thi ke thua toan bo thuoc tinh cua class cha,
+// trong truong hop muon thay doi thuoc tinh hoac method nao do thi chung ta co the ghi de
 
+class Animal {
+    constructor(name){
+        this.name = name
+    }
+    eat(){
+        console.log(`${this.name} dang an`);
+        
+    }
+}
 
+class Dog extends Animal {
+    constructor(name, color){
+        super(name) //goi contructor cua lop cha
+        this.color = color
+    }
+
+    bark(){
+        console.log(`Go go go`);
+        
+    }
+}
+
+const myDog = new Dog('Kitty', 'yellow')
+myDog.eat()
+
+myDog.bark()
+
+// 4. Tinh da hinh: cung mot hanh dong nhung thuc hien bang nhung cach khac nhau
+// VD cung la Di chuyen. Con nguoi thi di bang chan, ca boi bang vay, chim bay bang canh
+
+class Shape {
+    draw(){
+        console.log(`Ve hinh dang chung`);
+        
+    }
+}
+
+class Circle extends Shape {
+    draw(){
+        console.log(`Ve mot hinh tron`);
+        
+    }
+}
+
+class Square extends Shape {
+    draw(){
+        console.log(`Ve mot hinh vuong`);
+        
+    }
+}
+
+const shapes = [new Circle(), new Square(), new Shape()]
+
+shapes.forEach(shape => shape.draw())
 
