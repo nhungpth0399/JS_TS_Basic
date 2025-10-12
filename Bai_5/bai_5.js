d49741f1edbdd4242365a11becc928d9473fd122
nhungpth0399 <85088019+nhungpth0399@users.noreply.github.com>
2025-10-12 23:08:11 +0700

    push new code

---
+++ Bai_5/bai_5.js
@@ -0,0 +1,19 @@
+// function(tham so){}
+
+
+
+// const tenBien = function(thamso){
+//     return ket qua
+// }
+
+const tinhHieu = function(a, b){
+    return a - b
+}
+
+const ketQua = tinhHieu(8, 5)
+console.log(ketQua);
+
+
+
+
+
