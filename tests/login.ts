3e66652280f1d1046143fe77f7893edea94e1407
nhungpth0399 <85088019+nhungpth0399@users.noreply.github.com>
2025-10-29 23:40:22 +0700

    update

--- tests/login.ts
+++ tests/login.ts
@@ -1,6 +1,7 @@
 
-import { LoginPage } from "../pages/LoginPage.js";
-import { DataReader } from "../services/DataReader.js";
+
+import { LoginPage } from "../pages/LoginPage.ts";
+import { DataReader } from "../services/DataReader.ts";
 import type { ILoginData } from "../types/login-data.js";
 
 
@@ -14,6 +15,27 @@ async function runLoginTest() {
     if(loginCredentials.length === 0){
         console.log(`Khong co du lieu test de chay. Ket thuc`)
         return
+    }
+
+    let testCaseNumber = 1
+    
+    for(const credential of loginCredentials){
+        console.log(`Bat dau voi testcase ${testCaseNumber}, User: ${credential.username}`)
+        await loginPage.navigate()
+        await loginPage.login(credential)
+
+        const isPassed = await loginPage.verifyLoginResult(credential.expected)
+
+        if(isPassed){
+            console.log(`>>>> KET QUA PASSED`)
+        }else{
+            console.log(`>>>> KET QUA FAILED`);
+            
+        }
+
+        console.log(`KET THUC TEST CASE ${testCaseNumber}`);
+        
+        testCaseNumber++
     }
 
 }
